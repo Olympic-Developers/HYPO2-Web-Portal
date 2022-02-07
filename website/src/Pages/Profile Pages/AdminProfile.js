6a546ca4420fc533c9fa3fc631a2c1fc70c77d19
Peter Galvan <Pjg94@nau.edu>
2022-02-07 12:07:42 -0700

    Works!!!!!!!

--- website/src/Pages/Profile Pages/AdminProfile.js
+++ website/src/Pages/Profile Pages/AdminProfile.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Auth } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
 
 function App() {
+  // try to set this all off to a component used in all Profile pages
+
+  // Set default value for navigate
   let navigate = useNavigate();
 
-  // Make this a components when I have a chance
+  // Set constants
+  const authenticated = window.sessionStorage.getItem("authenticated");
+  const lowerCasedUserName = window.sessionStorage
+    .getItem("username")
+    .toLowerCase();
+  const lowerCasedClassification = window.sessionStorage
+    .getItem("classification")
+    .toLowerCase();
+
+  // useEffect used for checking for authenticated and proper classification
+  useEffect(() => {
+    // user not authenticated or not proper classification
+    if (authenticated !== "true" || lowerCasedClassification !== "admin") {
+      // user try to access this through a route in HYPO2 web Portal
+      if (authenticated !== "false") {
+        navigate(-1);
+      }
+      // Tried to access this page from a outside website
+      else {
+        navigate("/");
+      }
+    }
+  });
+
+  // For signing out users
   async function signOut() {
     try {
+      // signOut users
       await Auth.signOut();
-
+      // put user back on sign in page
       navigate("/");
-    } catch (error) {
-      console.log("error signing out: ", error);
+    } catch {
+      console.log("error signing out: ");
     }
   }
 
   return (
     <div>
-      <h1>Hello {Auth.user.username} welcome to your Staff page</h1>
+      <h1>Hello {lowerCasedUserName} welcome to your Admin Page</h1>
       <button
         onClick={() => {
           signOut();
--- website/src/Pages/SignIn.js
+++ website/src/Pages/SignIn.js
@@ -5,16 +5,29 @@ import awsExports from "../aws-exports";
 Amplify.configure(awsExports);
 
 function App() {
+  // set up use states for login information
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  // Set default value for authenticated and navigate
+  window.sessionStorage.setItem("authenticated", "false");
   let navigate = useNavigate();
 
+  // Function for signing in users
   async function signIn() {
     try {
+      // Getting user information
       const user = await Auth.signIn(username, password);
 
-      console.log(user);
+      // Storing information needed for user
+      window.sessionStorage.setItem("username", username);
+      window.sessionStorage.setItem("authenticated", "true");
+      window.sessionStorage.setItem(
+        "classification",
+        user.attributes["custom:Classification"]
+      );
 
+      // Checking for classification to redirecting user to correct profile page
       if (user.attributes["custom:Classification"] === "Staff") {
         navigate("/StaffProfile");
       } else if (user.attributes["custom:Classification"] === "Admin") {
@@ -24,13 +37,15 @@ function App() {
       } else {
         console.log("This Classification of account does not existed");
       }
-    } catch (error) {
-      console.log("error signing in", error);
+    } catch {
+      console.log("error signing in");
     }
   }
 
   return (
     <div>
+      <h1>Login Sign In To Your Account</h1>
+
       <div>
         <input
           type="text"
